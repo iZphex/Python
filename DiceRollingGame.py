d3875dca7ba788d11f5cf48a84704d5a63a090e1
Jose (Jay) Martinez <imjosemartinezjr@gmail.com>
2023-07-12 21:59:21 -0700

    DiceRollingGame.py

---
+++ DiceRollingGame.py
@@ -0,0 +1,15 @@
+# Import random
+# define a function roll the dice
+import random
+
+def roll_dice():
+    roll = input("Roll the dice (Yes/No): ")
+    while roll.lower() == "Yes".lower():
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+
+        print("dice rolled: {} and {}".format(dice1, dice2))
+
+        roll = input("Would you like to roll again? (Yes/No): ")
+
+roll_dice()
